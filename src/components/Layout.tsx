6bed76fc065959ea8213a7819f7958f521ea1749
JoshMDiaz <joshua.michael.diaz@gmail.com>
2025-10-13 21:07:35 -0600

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,11 +6,7 @@ interface LayoutProps {
   onDataRefresh?: () => void
 }
 
-export const Layout: React.FC<LayoutProps> = ({
-  children,
-  title,
-  onDataRefresh,
-}) => {
+export const Layout: React.FC<LayoutProps> = ({ children, title }) => {
   return (
     <div className='min-h-screen bg-gradient-to-b from-blue-50 via-white to-blue-50 pb-24 lg:pb-0'>
       <main className='px-4 py-5 lg:px-8 lg:py-8 lg:max-w-7xl lg:mx-auto'>
